84ad125636d9cb1e677a5c36f530f221c5c06fef
Omkar <omkarpatil3462@gmail.com>
2022-04-23 22:55:34 +0530

    modified

--- SinglyLL.c
+++ SinglyLL.c
@@ -11,6 +11,19 @@ typedef struct Node node;
 typedef struct Node *nodePtr;
 typedef struct Node **pnodePtr;
 
+int count(pnodePtr Head)
+{
+	int n = 0;
+
+	nodePtr temp = *Head;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		n++;
+	}
+	return n;
+}
+
 void DisplayList(pnodePtr Head)
 {
 
@@ -70,40 +83,101 @@ void InsertAtLast(pnodePtr Head, int num)
 	}
 }
 
+void DeleteAtLast(pnodePtr Head)
+{
+	if (*Head == NULL)
+	{
+		printf("List is empty\n");
+		return;
+	}
+
+	nodePtr temp = *Head;
+	while (temp->next->next != NULL)
+	{
+		temp = temp->next;
+	}
+	free(temp->next);
+	temp->next = NULL;
+}
+
+void DeleteAtFirst(pnodePtr Head)
+{
+	if (*Head == NULL)
+	{
+		printf("List is empty\n");
+		return;
+	}
+	else if ((*Head)->next == NULL)
+	{
+		*Head = NULL;
+	}
+
+	nodePtr temp = *Head;
+	*Head = (*Head)->next;
+	free(temp);
+}
+
 int main()
 {
-	int iChoice = 0;
-	int no;
+	int iChoice = 1;
+	int no, cnt;
 
 	nodePtr First = NULL;
-
-	printf("Please select the option: \n");
-	scanf("%d", &iChoice);
 
 	while (iChoice != 0)
 	{
 
+		printf("\n____________________________________\n");
+		printf("Enter your action on Linked list : \n");
+		printf("0 : to Exit the programme\n");
+		printf("1 : Display list\n");
+		printf("2 : Display Total number of list\n");
+		printf("3 : Insert node at First position\n");
+		printf("4 : Insert node at Last position\n");
+		printf("5 : Delete node at Last position\n");
+		printf("6 : Delete node at First position\n");
+
+		printf("\n____________________________________\n");
+		printf("Please select a option : \n");
+
+		scanf("%d", &iChoice);
+
 		switch (iChoice)
 		{
+		case 0:
+			printf("Thank you for using linked list.");
+			free(First);
+			break;
+
 		case 1:
 			DisplayList(&First);
 			break;
 
 		case 2:
-			printf("Please enter a number.\n");
-			scanf("%d", &no);
-			InsertAtFirst(&First, no);
+			cnt = count(&First);
+			printf("%d elements", cnt);
 			break;
 
 		case 3:
 			printf("Please enter a number.\n");
 			scanf("%d", &no);
+			InsertAtFirst(&First, no);
+			break;
+
+		case 4:
+			printf("Please enter a number.\n");
+			scanf("%d", &no);
 			InsertAtLast(&First, no);
 			break;
-		}
 
-		printf("1. Display list.\n2. Insert at first.\n3. Insert at Last.\n");
-		scanf("%d", &iChoice);
+		case 5:
+			DeleteAtLast(&First);
+			break;
+
+		case 6:
+			DeleteAtFirst(&First);
+			break;
+		}
 	}
 
 	return 0;
